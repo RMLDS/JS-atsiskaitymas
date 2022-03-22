6cb2e1241cc0c159184697018d6f8a9e9ba672bf
RMLDS <romualdas.bukauskas@live.com>
2022-03-22 12:15:45 +0200

    8 užduotis

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    sum (x, y) {
+        return x + y;
+    };
+    subtraction (x, y) {
+        return x - y;
+    };
+    multiplication (x, y) {
+        return x * y;
+    };
+    division (x, y) {
+        return Number((x / y).toFixed(3));
+    };
+};
+
+const paskaiciavimas = new Calculator;
+
+const sk1 = 5;
+const sk2 = 10;
+
+console.log("Sudėtis", paskaiciavimas.sum(sk1, sk2));
+console.log("Atimtis", paskaiciavimas.subtraction(sk1, sk2));
+console.log("Daugyba", paskaiciavimas.multiplication(sk1, sk2));
+console.log("Dalyba", paskaiciavimas.division(sk1, sk2));
