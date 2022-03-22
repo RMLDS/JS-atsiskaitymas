0a2242578a7c14ea8279962721c6af65610626ae
RMLDS <romualdas.bukauskas@live.com>
2022-03-22 08:38:29 +0200

    1 užduotis

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,26 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+document.querySelector("#submit-btn").addEventListener("click", e => {
+    e.preventDefault();
+    const kg = document.getElementById("search").value;
+    const lb = kg * 2.2046;
+    const g = kg / 0.0010000;
+    const oz = kg * 35.274;
+
+    document.getElementById("output").innerHTML = `
+        <div>
+            <h2>Svarais</h2>
+            <h4>${lb} lb</h4>
+        </div>
+        <div>
+            <h2>Gramais</h2>
+            <h4>${g} g</h4>
+        </div>
+        <div>
+            <h2>Uncijomis</h2>
+            <h4>${oz} oz</h4>
+        </div>
+    `;
+});
